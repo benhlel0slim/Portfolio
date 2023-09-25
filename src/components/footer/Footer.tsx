b00b2e54cb12f85442d653f21d94e25f667204e1
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-09-25 19:03:42 +0100

    feat: create and style footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Link from "next/link";
+import { EMAIL } from "@/constant/mail";
+import styles from "./footer.module.scss";
+import EmailLink from "../email/emailLink";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.container}>
+      <Link className={styles.email} href={EMAIL}>
+        <EmailLink />
+      </Link>
+      <p className={styles.text}>
+        Coded with 💻 and ☕ by <b>Selim Ben Hlel</b> Click
+        <Link href={"https://github.com/benhlel0slim/Portfolio"}> here </Link>
+        to check the source
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;

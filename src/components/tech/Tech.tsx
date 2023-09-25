3299f8e71b463993d423c68067e2f9ef0ba41adb
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-09-25 18:04:40 +0100

    feat: create and style tech component

--- src/components/tech/Tech.tsx
+++ src/components/tech/Tech.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import Tag from "../tag/Tag";
+import Arrow from "../arrow/Arrow";
+import styles from "./tech.module.scss";
+import ReactIcon from "../techIcons/React";
+import JavaScript from "../techIcons/JavaScript";
+import TypeScript from "../techIcons/TypeScript";
+import Css from "../techIcons/Css";
+import Recoil from "../techIcons/Recoil";
+import MaterialUi from "../techIcons/MaterialUi";
+import Cypress from "../techIcons/Cypress";
+import ReactQuery from "../techIcons/ReactQuery";
+import Eslint from "../techIcons/Eslint";
+import VsCode from "../techIcons/VsCode";
+import Figma from "../techIcons/Figma";
+import Vercel from "../techIcons/Vercel";
 
 function Tech() {
-  return <div>Tech</div>;
+  return (
+    <article id="about" className={styles.container}>
+      <div className={styles.openingTag}>
+        <Arrow className={styles.arrow} />
+        <Tag type="opening">Tech</Tag>
+      </div>
+      <div className={styles.text}>
+        <span className={styles.vl}></span>
+        <div className={styles.techIcons}>
+          <ReactIcon />
+          <JavaScript />
+          <TypeScript />
+          <Css />
+          <Recoil />
+          <MaterialUi />
+          <Cypress />
+          <ReactQuery />
+          <Vercel />
+          <Eslint />
+          <VsCode />
+          <Figma />
+        </div>
+      </div>
+      <div className={styles.closingTag}>
+        <Tag type="closing">Tech</Tag>
+      </div>
+    </article>
+  );
 }
 
 export default Tech;

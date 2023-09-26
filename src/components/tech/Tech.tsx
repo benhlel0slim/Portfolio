6c7be6c01295389992be2ff7df53e7780eca343e
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-09-26 14:20:50 +0100

    refactor: change name to Techs and styling

--- src/components/tech/Tech.tsx
+++ src/components/tech/Tech.tsx
@@ -1,48 +1,17 @@
+import { Techs } from "@/types/techs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import Tag from "../tag/Tag";
-import Arrow from "../arrow/Arrow";
 import styles from "./tech.module.scss";
-import ReactIcon from "../techIcons/React";
-import JavaScript from "../techIcons/JavaScript";
-import TypeScript from "../techIcons/TypeScript";
-import Css from "../techIcons/Css";
-import Recoil from "../techIcons/Recoil";
-import MaterialUi from "../techIcons/MaterialUi";
-import Cypress from "../techIcons/Cypress";
-import ReactQuery from "../techIcons/ReactQuery";
-import Eslint from "../techIcons/Eslint";
-import VsCode from "../techIcons/VsCode";
-import Figma from "../techIcons/Figma";
-import Vercel from "../techIcons/Vercel";
 
-function Tech() {
+function Tech({ name, src, url }: Techs) {
   return (
-    <article id="about" className={styles.container}>
-      <div className={styles.openingTag}>
-        <Arrow className={styles.arrow} />
-        <Tag type="opening">Tech</Tag>
-      </div>
-      <div className={styles.text}>
-        <span className={styles.vl}></span>
-        <div className={styles.techIcons}>
-          <ReactIcon />
-          <JavaScript />
-          <TypeScript />
-          <Css />
-          <Recoil />
-          <MaterialUi />
-          <Cypress />
-          <ReactQuery />
-          <Vercel />
-          <Eslint />
-          <VsCode />
-          <Figma />
-        </div>
-      </div>
-      <div className={styles.closingTag}>
-        <Tag type="closing">Tech</Tag>
-      </div>
-    </article>
+    <div className={styles.container}>
+      <Link href={url} target="_blank">
+        <Image src={src} alt={`logo of ${name}`} width={120} height={120} />
+        <p>{name}</p>
+      </Link>
+    </div>
   );
 }
 

e1cb47679ac7418b0dd957bfeabe0307f68d6268
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-10-02 19:59:36 +0100

    refactor: modify and style projects components

---
+++ src/components/projects/Projects.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useRef } from "react";
+import Tag from "../tag/Tag";
+import Arrow from "../arrow/Arrow";
+import styles from "./projects.module.scss";
+import { PROJECTS } from "@/constant/projects";
+import Project from "../project/Project";
+import { useIsVisible } from "@/hook/useIsVisible";
+
+function Projects() {
+  const ref = useRef<HTMLElement>(null);
+  const { hasIntersected } = useIsVisible(ref);
+  return (
+    <article ref={ref} id="projects" className={styles.container}>
+      <div className={styles.openingTag}>
+        <Arrow
+          className={`${styles.arrow} ${hasIntersected ? styles.animate : ""}`}
+        />
+        <Tag type="opening">Projects</Tag>
+      </div>
+      <div className={`${styles.text} ${hasIntersected ? styles.animate : ""}`}>
+        <span className={styles.vl} />
+        {PROJECTS.map((project) => (
+          <Project {...project} key={project.title} />
+        ))}
+      </div>
+      <div
+        className={`${styles.closingTag} ${
+          hasIntersected ? styles.animate : ""
+        }`}
+      >
+        <Tag type="closing">Projects</Tag>
+      </div>
+    </article>
+  );
+}
+
+export default Projects;

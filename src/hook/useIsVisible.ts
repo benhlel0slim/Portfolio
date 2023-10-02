cfcfa2afd36d617b45f0c58f55cadb932095b822
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-10-02 19:46:29 +0100

    feat: create hook for scroll animation

---
+++ src/hook/useIsVisible.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export function useIsVisible(ref: React.RefObject<HTMLElement>) {
+  const [isIntersecting, setIntersecting] = useState(false);
+  const [hasIntersected, setHasIntersected] = useState(false);
+
+  useEffect(() => {
+    if (!ref.current) return;
+    const observer = new IntersectionObserver(([entry]) =>
+      setIntersecting(entry.isIntersecting)
+    );
+
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  useEffect(() => {
+    if (isIntersecting && !hasIntersected) {
+      setHasIntersected(true);
+      console.log("updating");
+    }
+  }, [isIntersecting, hasIntersected]);
+
+  return { isIntersecting, hasIntersected };
+}

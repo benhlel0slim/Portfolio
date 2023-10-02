2404cadfe36a95dc2d128dd14522b3998d7fb086
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-10-02 20:00:09 +0100

    feat: adding on scroll animation and styling

--- src/components/techs/Techs.tsx
+++ src/components/techs/Techs.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import Tag from "../tag/Tag";
 import Arrow from "../arrow/Arrow";
 import styles from "./techs.module.scss";
 import Tech from "../tech/Tech";
 import { TECHS } from "@/constant/techIcons";
+import { useIsVisible } from "@/hook/useIsVisible";
 
 function Techs() {
+  const ref = useRef<HTMLElement>(null);
+  const { hasIntersected } = useIsVisible(ref);
   return (
-    <article id="tech" className={styles.container}>
+    <article id="tech" ref={ref} className={styles.container}>
       <div className={styles.openingTag}>
-        <Arrow className={styles.arrow} />
+        <Arrow
+          className={`${styles.arrow} ${hasIntersected ? styles.animate : ""}`}
+        />
         <Tag type="opening">Tech</Tag>
       </div>
-      <div className={styles.text}>
+      <div className={`${styles.text} ${hasIntersected ? styles.animate : ""}`}>
         <span className={styles.vl}></span>
         <div className={styles.techIcons}>
           {TECHS.map(({ name, src, url }) => (
@@ -20,7 +26,11 @@ function Techs() {
           ))}
         </div>
       </div>
-      <div className={styles.closingTag}>
+      <div
+        className={`${styles.closingTag} ${
+          hasIntersected ? styles.animate : ""
+        }`}
+      >
         <Tag type="closing">Tech</Tag>
       </div>
     </article>

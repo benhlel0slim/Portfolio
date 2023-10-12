d63230b900cd3123b7f1e43f80bbce04466e4197
selim <94851678+benhlel0slim@users.noreply.github.com>
2023-10-12 21:12:54 +0100

    refactor: styling techs component

--- src/components/tech/Tech.tsx
+++ src/components/tech/Tech.tsx
@@ -8,7 +8,7 @@ function Tech({ name, src, url }: Techs) {
   return (
     <div className={styles.container}>
       <Link href={url} target="_blank">
-        <Image src={src} alt={`logo of ${name}`} width={150} height={150} />
+        <Image src={src} alt={`logo of ${name}`} width={100} height={100} />
         <p>{name}</p>
       </Link>
     </div>

3d0d936fee917fe57b1fb55795f9dc6bc69a7e4e
selim <94851678+benhlel0slim@users.noreply.github.com>
2023-10-12 15:25:57 +0100

    refactor: navbar styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
+import styles from "./page.module.css";
 import About from "@/components/about/About";
 import Footer from "@/components/footer/Footer";
-import Landing from "@/components/homePage/Landing";
+import Landing from "@/components/landing/Landing";
 import NavBar from "@/components/navBar/Navbar";
 import Projects from "@/components/projects/Projects";
 import Techs from "@/components/techs/Techs";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <NavBar />
       <Landing />
       <About />

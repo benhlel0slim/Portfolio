3a7392075b5b878651ed85d0781ab36ec07d399c
selim <94851678+benhlel0slim@users.noreply.github.com>
2023-12-28 00:38:38 +0100

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,14 @@ import NavBar from "@/components/navBar/Navbar";
 import Projects from "@/components/projects/Projects";
 import Techs from "@/components/techs/Techs";
 import ReactGA from "react-ga";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
     ReactGA.initialize("G-FKQLHFB3VJ");
     ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
+
   return (
     <main className={styles.main}>
       <NavBar />

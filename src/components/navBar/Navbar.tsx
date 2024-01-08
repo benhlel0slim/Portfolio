b7dd6cac80fbbb27ba178e1fac9512c56f85f788
selim <94851678+benhlel0slim@users.noreply.github.com>
2024-01-08 23:31:29 +0100

    feat: add Internationalization to  navbar component

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -3,12 +3,15 @@ import navIcon from "../../../public/assets/navIcon.svg";
 import Link from "next/link";
 import styles from "./navBar.module.scss";
 import Tag from "../tag/Tag";
+import LocaleSwitcher from "../localeSwitcher/LocaleSwitcher";
+import { DictionaryProps } from "@/types/dictionary";
+import { Locale } from "../../../i18n.config";
 
-function NavBar() {
+function NavBar({ data, lang }: { data: DictionaryProps; lang: Locale }) {
   return (
     <header className={styles.container}>
       <h1>
-        <Link href="/">
+        <Link href="/en">
           <Image className={styles.logo} src={navIcon} alt="logo" />
         </Link>
       </h1>
@@ -16,18 +19,21 @@ function NavBar() {
         <ul className={styles.links}>
           <li>
             <Link href="#about">
-              <Tag type="self-closing">About </Tag>
+              <Tag type="self-closing">{data.navbar.about} </Tag>
             </Link>
           </li>
           <li>
             <Link href="#projects">
-              <Tag type="self-closing">Projects </Tag>
+              <Tag type="self-closing">{data.navbar.project} </Tag>
             </Link>
           </li>
           <li>
             <Link href="#tech">
-              <Tag type="self-closing">Tech</Tag>
+              <Tag type="self-closing">{data.navbar.tech}</Tag>
             </Link>
+          </li>
+          <li>
+            <LocaleSwitcher lang={lang} />
           </li>
         </ul>
       </nav>

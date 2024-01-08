e9db679ed9ae14776daff8d7e44fa6b3121f1206
selim <94851678+benhlel0slim@users.noreply.github.com>
2024-01-08 23:54:42 +0100

    refactor: badge component props type

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -3,7 +3,13 @@ import { Unpacked } from "@/types/utils";
 import React, { CSSProperties } from "react";
 import styles from "./bagde.module.scss";
 
-function Badge({ background, color, name }: Unpacked<Project["techs"]>) {
+function Badge({
+  background,
+  color,
+  name,
+}:
+  | Unpacked<Project["en"]>["techs"][number]
+  | Unpacked<Project["fr"]>["techs"][number]) {
   return (
     <div
       className={styles.badge}

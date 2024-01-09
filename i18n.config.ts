c2ed830a4270efad652a90f1839b2aa1988bf716
selim <94851678+benhlel0slim@users.noreply.github.com>
2024-01-09 23:35:43 +0100

    refactor: i18n type

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,6 +1,9 @@
-export const i18n = {
-  defaultLocale: "en",
-  locales: ["en", "fr"],
-} as const;
+export enum Locale {
+  English = "en",
+  French = "fr",
+}
 
-export type Locale = (typeof i18n)["locales"][number];
+export const i18n = {
+  locales: [Locale.English, Locale.French],
+  defaultLocale: Locale.English,
+};

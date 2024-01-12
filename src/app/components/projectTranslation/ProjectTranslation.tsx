848718ab3880b0183f69d330d5ccb761bbfb4e76
selim <94851678+benhlel0slim@users.noreply.github.com>
2024-01-12 01:03:18 +0100

    refactor: ProjectTranslation component

---
+++ src/app/components/projectTranslation/ProjectTranslation.tsx
@@ -0,0 +1,18 @@
+import { PROJECTS } from "@/app/constant/projects";
+import { Locale } from "../../../../i18n.config";
+import Project from "../project/Project";
+import { Project as Props } from "@/app/types/project";
+
+const ProjectTranslation = ({ lang }: { lang: Locale }) => {
+  return (
+    <>
+      {PROJECTS[lang].map(
+        (project: Props["en"][number] | Props["fr"][number]) => (
+          <Project {...project} key={project.title} />
+        )
+      )}
+    </>
+  );
+};
+
+export default ProjectTranslation;

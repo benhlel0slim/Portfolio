22ccbabfe1287b2ea72c43286635b009c3c5daaf
selim <94851678+benhlel0slim@users.noreply.github.com>
2024-02-05 19:34:15 +0100

    feat: add couscous project and styling projects

--- src/app/components/projectTranslation/ProjectTranslation.tsx
+++ src/app/components/projectTranslation/ProjectTranslation.tsx
@@ -3,16 +3,9 @@ import { Locale } from "../../../../i18n.config";
 import Project from "../project/Project";
 import { Project as Props } from "@/app/types/project";
 
-const ProjectTranslation = ({ lang }: { lang: Locale }) => {
-  return (
-    <>
-      {PROJECTS[lang].map(
-        (project: Props["en"][number] | Props["fr"][number]) => (
-          <Project {...project} key={project.title} />
-        )
-      )}
-    </>
-  );
-};
+const ProjectTranslation = ({ lang }: { lang: Locale }) =>
+  PROJECTS[lang].map((project: Props["en"][number] | Props["fr"][number]) => (
+    <Project {...project} key={project.title} />
+  ));
 
 export default ProjectTranslation;
